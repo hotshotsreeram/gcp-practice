b6cdade95459234de65c69ad43b37bee5712a5dc
Sreeram <hotshotsreeram@gmail.com>
2023-09-21 12:49:01 -0400

    updating gke-terraform

--- gke-terraform/provider.tf
+++ gke-terraform/provider.tf
@@ -1,5 +1,4 @@
 provider "google" {
-  credentials = file("/home/sreeramvellanki/sreeram_training/gcp-practice/terraform-gcp-serviceaccount-key.json")
   project     = "robotic-circle-359714"
   region      = "northamerica-northeast2"  # Change to your desired region
 }
