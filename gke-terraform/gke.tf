fde2f3c25d006a790505fad474aee1e46a62da95
Sreeram <hotshotsreeram@gmail.com>
2023-09-27 20:22:11 -0400

    testing jenkins pipeline

--- gke-terraform/gke.tf
+++ gke-terraform/gke.tf
@@ -2,7 +2,7 @@ resource "google_container_cluster" "my_cluster" {
   name     = var.name
   location = var.location
   node_pool {
-    name       = "node-pool"
+    name       = "my-node-pool"
     initial_node_count = 1
     # Specify other node pool settings as needed
   }
