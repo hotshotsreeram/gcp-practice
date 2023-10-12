59645f5e47d3dc1e6466e3317b83b23631347633
Sreeram <hotshotsreeram@gmail.com>
2023-10-12 10:03:27 -0400

    deploying gke cluster

--- gke-terraform/gke.tf
+++ gke-terraform/gke.tf
@@ -5,6 +5,11 @@ resource "google_container_cluster" "my_cluster" {
     name       = "my-node-pool"
     initial_node_count = 1
     # Specify other node pool settings as needed
+    node_config {
+      machine_type = "n1-standard-2"  # Specify the machine type (e.g., n1-standard-2)
+      disk_size_gb = 20  # Allocate 20GB of storage for each node
+      # Specify other node configuration settings as needed
+    }
   }
 }
 
